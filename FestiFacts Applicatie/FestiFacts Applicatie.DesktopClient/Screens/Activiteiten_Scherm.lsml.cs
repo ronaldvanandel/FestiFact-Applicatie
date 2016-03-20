2ef950689a53abfc11a734e142b247579146c39e
Ronald van Andel <ra87757@outlook.com>
2016-03-20 15:07:37 +0100

    Commited by Ronald van Andel to team up our efforts.

---
+++ FestiFacts Applicatie/FestiFacts Applicatie.DesktopClient/Screens/Activiteiten_Scherm.lsml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.LightSwitch.Presentation.Extensions;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.IO;
+using System.Linq;
+using System;
+
+namespace LightSwitchApplication
+{
+    public partial class Activiteiten_Scherm
+    {
+    }
+}
